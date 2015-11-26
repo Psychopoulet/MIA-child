a2493e0d139479ffd681f3b8c813ff479f215e95
Psychopoulet <svidal@wynd.eu>
2015-11-26 14:17:40 +0100

    suppression des plugins médias

--- class/MIA-Child.js
+++ class/MIA-Child.js
@@ -5,6 +5,7 @@
 		path = require('path'),
 		fs = require('fs'),
 		q = require('q'),
+		exec = require('child_process').exec,
 
 		Container = require(path.join(__dirname, 'Container.js')),
 		Logs = require(path.join(__dirname, 'Logs.js'));
@@ -48,13 +49,93 @@
 
 									})
 									.catch(deferred.reject);
-											
-
+										
 							// start
 								
 								Container.get('server.socket.mia').start()
 									.then(deferred.resolve)
 									.catch(deferred.reject);
+
+							// sockets
+								
+								Container.get('server.socket.mia')
+									.onDisconnect(function (socket) {
+
+										socket.removeAllListeners('child.videos.play');
+										socket.removeAllListeners('child.sounds.play');
+										
+									})
+									.onConnection(function (socket) {
+
+										socket.on('child.videos.play', function(video) {
+
+											try {
+
+												if (!video.url || '' == video.url) {
+													socket.emit('child.videos.error', 'Url missing');
+												}
+												else {
+
+													exec('vlc "' + video.url + '" --play-and-exit', function (err, stdout, stderr) {
+
+														if (err) {
+															socket.emit('child.videos.error', (err.message) ? err.message : err);
+														}
+														else {
+															socket.emit('child.videos.played', video);
+														}
+
+													});
+
+												}
+
+											}
+											catch(e) {
+												socket.emit('child.videos.error', (e.message) ? e.message : e);
+											}
+					
+										})
+										.on('child.sounds.play', function(sound) {
+
+											try {
+
+												if (!sound.url || '' == sound.url) {
+													socket.emit('child.sounds.error', 'Url missing');
+												}
+												else {
+
+													exec('cvlc "' + sound.url + '" --play-and-exit', function (err, stdout, stderr) {
+
+														if (!err) {
+															socket.emit('child.sounds.played', sound);
+														}
+														else {
+
+															exec('vlc "' + sound.url + '" --play-and-exit', function (err, stdout, stderr) {
+
+																if (err) {
+																	socket.emit('child.sounds.error', (err.message) ? err.message : err);
+																}
+																else {
+																	socket.emit('child.sounds.played', sound);
+																}
+
+															});
+
+														}
+								
+													});
+
+												}
+
+											}
+											catch(e) {
+												socket.emit('child.sounds.error', (e.message) ? e.message : e);
+											}
+					
+										});
+
+									});
 									
 						}
 						catch (e) {
